8297136a3209b92e1f04e4d34f0c59e88d2cdc81
Bastien <bureaubastien3@gmail.com>
2024-03-04 10:03:35 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using SocketBackend;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerGameOfLife
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 5555; // Port sur lequel le serveur écoute
+
+            SocketServer server = new SocketServer(port);
+            await server.StartAsync();
+        }
+    }
+}
